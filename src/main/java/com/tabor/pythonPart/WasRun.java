f418e2e1f07992a947040386f5ace20556b15610
rivalez <marektabor1@gmail.com>
2017-11-07 09:30:13 +0100

    teardown works

--- src/main/java/com/tabor/pythonPart/WasRun.java
+++ src/main/java/com/tabor/pythonPart/WasRun.java
@@ -4,10 +4,29 @@ package com.tabor.pythonPart;
  * Created by marek on 06.11.2017.
  */
 public class WasRun {
+
     boolean wasRun;
+    String log;
 
     boolean run(){
-        return this.wasRun = true;
+        setUp();
+        testMethod();
+        tearDown();
+        return this.wasRun = Boolean.TRUE;
+    }
+
+    boolean setUp() {
+        this.log = "setUp ";
+        return Boolean.TRUE;
+    }
+
+    void testMethod(){
+        this.wasRun = Boolean.TRUE;
+        this.log = log + "testMethod ";
+    }
+
+    void tearDown(){
+        this.log = log + "tearDown ";
     }
 
 }
--- src/test/java/com/tabor/pythonPart/PythonTest.java
+++ src/test/java/com/tabor/pythonPart/PythonTest.java
@@ -1,8 +1,10 @@
 package com.tabor.pythonPart;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -11,18 +13,37 @@ import static org.junit.Assert.assertTrue;
  */
 public class PythonTest {
 
-    @Test
-    public void wasRun(){
-        WasRun wasRun = new WasRun();
-        assertFalse(wasRun.wasRun);
-        assertTrue(wasRun.run());
+    private WasRun wasRun = new WasRun();
+
+    @Before
+    public void setUp() {
+        wasRun = new WasRun();
     }
 
     @Test
-    public void testCaseTest(){
+    public void wasRun(){
+        assertFalse(wasRun.wasRun);
+    }
+
+    @Test
+        public void testCaseTest(){
         WasRun wasRun = Mockito.mock(WasRun.class);
         Mockito.verify(wasRun, Mockito.times(0)).run();
         wasRun.run();
         Mockito.verify(wasRun, Mockito.times(1)).run();
+    }
+
+    @Test
+    public void testSetUp(){
+        wasRun.setUp();
+        wasRun.testMethod();
+        assertTrue(wasRun.log.equals("setUp testMethod "));
+    }
+
+    @Test
+    public void testTemplateMethod(){
+        wasRun.testMethod();
+        wasRun.run();
+        assertTrue(wasRun.log.equals("setUp testMethod tearDown "));
     }
 }
